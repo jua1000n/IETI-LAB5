a2e0659067ea1a9c4ed35007f1b943de860c508e
Juan Cadavid <jua1000n@gmail.com>
2022-09-21 21:05:50 -0500

    Lab5Completo

---
+++ IETI-LAB5/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+        <div>
+          <h1>Login</h1>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="button"> Logear</button>
+        </div>
+      </main>
+    );
+  }
+
+export default App;
